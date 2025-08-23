b1bc6aabaf401f853fa50a320c29423d474ff62b
kuhelii <constantkuheli5@gmail.com>
2025-08-23 14:10:00 +0530

    meow

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -24,7 +24,7 @@ export const Navigation: React.FC = () => {
 
   const handleLogout = () => {
     removeTokens();
-    toast.success("Logged out successfully");
+    toast.success("Logged out successfully meow");
     navigate("/login");
   };
 
